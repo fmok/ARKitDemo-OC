5ae0770be76e270bd4620df0bae57d2a8e97ac2c
fm <wangjiuyin@jiemian.com>
2017-10-18 10:43:28 +0800

    Initial Commit

---
+++ ARKitDemo-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARKitDemo-OC
+//
+//  Created by fm on 2017/10/18.
+//  Copyright © 2017年 wangjiuyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
