ac3c0bc32c6361dce92495596b4568fb2906356b
fm <wangjiuyin@jiemian.com>
2017-10-19 11:30:34 +0800

    init

--- ARKitDemo-OC/ViewController.m
+++ ARKitDemo-OC/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "ARSCNViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UIButton *startBtn;
 
 @end
 
@@ -17,8 +20,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    WS(weakSelf);
+    [self.view addSubview:self.startBtn];
+    [self.startBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(weakSelf.view);
+    }];
+    [self.startBtn sizeToFit];
 }
 
+#pragma mark - Events
+- (void)clickStart:(UIButton *)sender
+{
+    ARSCNViewController *scnVC = [[ARSCNViewController alloc] init];
+    [self.navigationController pushViewController:scnVC animated:YES];
+}
+
+#pragma mark - getter
+- (UIButton *)startBtn
+{
+    if (!_startBtn) {
+        _startBtn = [[UIButton alloc] initWithFrame:CGRectZero];
+        [_startBtn setTitle:@"Begin the exploration of the universe" forState:UIControlStateNormal];
+        [_startBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [_startBtn addTarget:self action:@selector(clickStart:) forControlEvents:UIControlEventTouchUpInside];
+        _startBtn.titleLabel.font = [UIFont systemFontOfSize:20.f];
+    }
+    return _startBtn;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
